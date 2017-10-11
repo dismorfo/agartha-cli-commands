8637c152dbcfa5eddea534ca034f9b8ec25b0bf8
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-10-11 12:04:18 -0400

    commands

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 'use strict'
 
 const Commands = class {
-  
-  constructor () {
+
+  constructor (program) {
     this.version = '1.0.1'
     this.name = 'agartha-cli-commands'
+    this.commands = []
+    this.options = []
+    const agartha = process.agartha
+    const defaultCommands = agartha.path.join(__dirname, 'lib/commands')
+    const localCommands = agartha.path.join(agartha.appDir(), 'app', 'commands')
+    // need a better solution for options
+    this.listOptions().forEach((option) => {
+      program.option(option.flag, option.description)
+    })
+    agartha._.each([defaultCommands, localCommands], (path) => {
+      if (agartha.exists(path)) {
+        agartha._.each(agartha.readdirSync(path), (directory) => {
+          const exists = agartha.path.join(path, directory, 'index.js')
+          if (agartha.exists(exists)) {
+            const SubCommand = require(exists)
+            const subcommand = new SubCommand()
+            program.command(subcommand.command)
+                   .description(subcommand.description)
+                   .alias(subcommand.alias)
+                   .action(subcommand.action)
+            
+          }
+        })
+      }
+    })
+    program.parse(process.argv)
   }
 
   listOptions () {
@@ -29,30 +55,6 @@ const Commands = class {
     ]
   }
 
-  listCommands () {
-    let commands = []
-    const agartha = process.agartha
-    const defaultCommands = agartha.path.join(__dirname, 'lib/commands')
-    const localCommands = agartha.path.join(agartha.appDir(), 'app', 'commands')
-    agartha._.each([defaultCommands, localCommands], (path) => {
-      if (agartha.exists(path)) {
-        agartha._.each(agartha.readdirSync(path), (directory) => {
-          const exists = agartha.path.join(path, directory, 'index.js')
-          if (agartha.exists(exists)) {
-            const command = require(exists)
-            agartha._.each(agartha._.keys(command), (key) => {
-              commands.push({
-                'command': key,
-                'description': command[key].description,
-                'action':  command[key].action
-              })
-            })
-          }
-        })
-      }
-    })
-    return commands
-  }
 }
 
 exports = module.exports = Commands
