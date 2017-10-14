c7dc082450adde7afa09e89255ef9a6b77b0a721
Alberto Ortiz Flores <pluto@pluto.local>
2017-10-14 15:47:02 -0400

    try/catch

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
 'use strict'
 
 const Commands = class {
-
   constructor (program) {
     this.version = '1.0.1'
     this.name = 'agartha-cli-commands'
-    const agartha = process.agartha
-    const defaultCommands = agartha.path.join(__dirname, 'lib/commands')
-    const localCommands = agartha.path.join(agartha.appDir(), 'app', 'commands')
-    this.defaultOptions.forEach((option) => {
-      program.option(option.flag, option.description)
-    })
-    agartha._.each([defaultCommands, localCommands], (path) => {
-      if (agartha.exists(path)) {
-        agartha._.each(agartha.readdirSync(path), (directory) => {
-          const exists = agartha.path.join(path, directory, 'index.js')
-          if (agartha.exists(exists)) {
-            const SubCommand = require(exists)
-            const subcommand = new SubCommand()
-            subcommand.options.forEach((option) => {
-              program.option(option.flag, option.description)
-            })
-            program.command(subcommand.command)
-                   .description(subcommand.description)
-                   .alias(subcommand.alias)
-                   .action(subcommand.action)
-          }
-        })
-      }
-    })
-    program.parse(process.argv)
+    try {
+      const agartha = process.agartha
+      const defaultCommands = agartha.path.join(__dirname, 'lib/commands')
+      const localCommands = agartha.path.join(agartha.appDir(), 'app', 'commands')
+      this.defaultOptions.forEach((option) => {
+        program.option(option.flag, option.description)
+      })
+      agartha._.each([defaultCommands, localCommands], (path) => {
+        if (agartha.exists(path)) {
+          agartha._.each(agartha.readdirSync(path), (directory) => {
+            const exists = agartha.path.join(path, directory, 'index.js')
+            if (agartha.exists(exists)) {
+              const SubCommand = require(exists)
+              const subcommand = new SubCommand()
+              subcommand.options.forEach((option) => {
+                program.option(option.flag, option.description)
+              })
+              program.command(subcommand.command)
+                .description(subcommand.description)
+                .alias(subcommand.alias)
+                .action(subcommand.action)
+            }
+          })
+        }
+      })
+      program.parse(process.argv)
+    } catch (e) {
+      console.error('outer', e.message)
+    }
   }
 
   get defaultOptions () {
@@ -40,7 +43,6 @@ const Commands = class {
       }
     ]
   }
-
 }
 
 exports = module.exports = Commands
