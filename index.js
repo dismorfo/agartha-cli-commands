4239c3135a151fee10b5c6980f29f12056acf159
Alberto Ortiz Flores <aortiz@nyu.edu>
2017-10-22 13:04:38 -0400

    updates

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict'
 const Commands = class {
   constructor (program) {
-    this.version = '1.0.6'
+    this.version = '1.0.8'
     this.name = 'agartha-cli-commands'
     try {
       const agartha = process.agartha
       const defaultCommands = agartha.path.join(__dirname, 'lib/commands')
       const localCommands = agartha.path.join(agartha.appDir(), 'app', 'commands')
-      agartha._.each([defaultCommands, localCommands], (path) => {
+      let relicCommands = null
+      if (agartha.relic()) {
+        relicCommands = agartha.relicDir()
+      }
+      agartha._.each([defaultCommands, relicCommands, localCommands], (path) => {
         if (agartha.exists(path)) {
           agartha._.each(agartha.readdirSync(path), (directory) => {
             const exists = agartha.path.join(path, directory, 'index.js')
